26d8a658b58c59ea8ff3f70cbff24077b7c09a57
f-l7 <ateyahali432@gmail.com>
2025-04-29 19:23:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function submitForm() {
     btn.textContent = "جاري الإرسال...";
 
     try {
-        const response = await fetch("WEBHOOK_URL_HERE", {
+        const response = await fetch("https://discord.com/api/webhooks/1366369025986265179/rVX34EBkGn6anyrTz_IMJgBG1Acjr43_raqun2XVkTtpkSeFmygPcYwuL1aebfaQGJp4", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
