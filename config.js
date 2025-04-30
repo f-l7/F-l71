01974a65cc35037a0cb211bbf069f55ba6d6eee4
f-l7 <ateyahali432@gmail.com>
2025-04-30 13:02:41 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ADMIN_USERNAME: "admin",
+    ADMIN_PASSWORD: "admin123",
+    PORT: 3000,
+    SESSION_SECRET: "your_secret_key_here",
+    DISCORD_BOT_TOKEN: "your_discord_bot_token_here",
+    DISCORD_ADMIN_CHANNEL: "your_admin_channel_id_here"
+};
